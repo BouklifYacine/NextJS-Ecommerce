b3078cc1bd19e7df057a25d36764092603b343d1
YacineBoukliff <yacine69800@live.fr>
2025-04-07 11:11:41 +0200

    Ajout de la route GET pour récupérer les produits par l'admin

--- E-commerce/app/api/admin/produits/route.ts
+++ E-commerce/app/api/admin/produits/route.ts
@@ -5,6 +5,39 @@ import { NextRequest, NextResponse } from "next/server";
 import { reponseApiProduit } from "./(interface-types)/interface";
 import { AccesAdmin } from "@/lib/SessionAdmin";
 
+export async function GET(request: NextRequest) {
+  const Admin = await AccesAdmin();
+  
+  if (Admin !== true) { return Admin }
+
+  try {
+    // Récupérer tous les produits avec leurs images
+    const produits = await prisma.produit.findMany({
+      include: {
+        images: true
+      },
+      orderBy: {
+        createdAt: 'desc'
+      }
+    });
+
+    if(produits.length === 0) {
+      return NextResponse.json(
+        {message: "Pas de produits disponible"}, 
+        {status: 404} 
+      );
+    }
+
+    return NextResponse.json({ produits });
+  } catch (error) {
+    console.error("Erreur lors de la récupération des produits:", error);
+    
+    return NextResponse.json(
+      { message: "Une erreur est survenue lors de la récupération des produits" },
+      { status: 500 }
+    );
+  }
+}
 
 export async function POST(request: NextRequest) {
     const Admin = await AccesAdmin();
