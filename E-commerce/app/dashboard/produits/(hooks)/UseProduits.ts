3ebb0bc95ab16d21debc764e7d634e5cddcab4a3
YacineBoukliff <yacine69800@live.fr>
2025-04-10 15:41:03 +0200

    Hook pour supprimer un produit place a l'implémentation sur le composant

--- E-commerce/app/dashboard/produits/(hooks)/UseProduits.ts
+++ E-commerce/app/dashboard/produits/(hooks)/UseProduits.ts
@@ -1,6 +1,7 @@
 import { reponseApiProduit } from "@/app/api/admin/produits/(interface-types)/interface";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export function useProduits() {
     return useQuery<reponseApiProduit[]>({  
@@ -10,4 +11,33 @@ export function useProduits() {
             return response.data.produits; 
         }
     });
+}
+
+
+export function useSupprimerProduits(){
+    const QueryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn : async (produitId : string) => {
+            const response = await axios.delete(`/api/admin/produits/${produitId}`)
+            return response.data
+        },
+        onMutate: async (produitId) => {
+            await QueryClient.cancelQueries({ queryKey: ['produits'] });
+            
+            const AnciensProduits = QueryClient.getQueryData(['produits']);
+            QueryClient.setQueryData(['produits'], (ListeActuelle : reponseApiProduit[]) => 
+                ListeActuelle.filter(produit => produit.id !== produitId)
+            );
+            return { AnciensProduits };
+          },
+        
+        onSettled: () => {
+            QueryClient.invalidateQueries({ queryKey: ['produits'] });
+          },
+        onSuccess : () => {
+            toast.success("Produit supprimé avec succès")
+        }
+    })
+
 }
