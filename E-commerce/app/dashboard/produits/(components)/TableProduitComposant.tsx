6f9b9e07e39b11decf120c99186fa744858bff02
YacineBoukliff <yacine69800@live.fr>
2025-04-10 10:32:59 +0200

    Ajout des valeur statiques et du rendu conditionnel du style pour chaque élément du tableau place au data fetch

--- E-commerce/app/dashboard/produits/(components)/TableProduitComposant.tsx
+++ E-commerce/app/dashboard/produits/(components)/TableProduitComposant.tsx
@@ -1,23 +1,41 @@
+" use client"
+
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { Checkbox } from '@/components/ui/checkbox'
-
 import React from 'react'
+import { Badge } from '@/components/ui/badge'
+
+const QuantiteStock : number = 11
+
+let prixPromotion ;
+
+const promotion : boolean = true 
+
+const GestionduStock = () => {
+
+  if(QuantiteStock <= 10) return "bg-red-500 text-white"
+  else if (QuantiteStock < 50) return "bg-yellow-500 text-white"
+  else return "bg-green-500 text-white"
+}
 
 const TableProduitComposant = () => {
   return (
-    <div className="rounded-md border p-6 mt-6">
-      <h1 className='text-xl font-bold mb-4'>Liste des produits </h1>
-      <Table>
+    <div className="rounded-md  p-6 mt-6">
+      <h1 className='text-xl font-bold m-4 text-center'>Liste des produits </h1>
+      <Table className='rounded-3xl'>
         <TableHeader>
           <TableRow>
             <TableHead></TableHead>
-            <TableHead className="font-bold text-black">Avatar</TableHead>
-            <TableHead className="font-bold text-black">Role</TableHead>
-            <TableHead className="font-bold text-black">Pseudo</TableHead>
-            <TableHead className="font-bold text-black">Email</TableHead>
-            <TableHead className="font-bold text-black">Créé le</TableHead>
-            <TableHead className="font-bold text-black">Abonnement</TableHead>
-            <TableHead className="font-bold text-black">Periode</TableHead>
+            <TableHead className="font-bold text-black">Nom</TableHead>
+            <TableHead className="font-bold text-black">Prix </TableHead>
+            <TableHead className="font-bold text-black">Prix en Promotion</TableHead>
+            <TableHead className="font-bold text-black">Quantité Stock</TableHead>
+            <TableHead className="font-bold text-black">En Promotion</TableHead>
+            <TableHead className="font-bold text-black">Catégorie</TableHead>
+            <TableHead className="font-bold text-black"> Images</TableHead>
+            <TableHead className="font-bold text-black"> Avis</TableHead>
+            <TableHead className="font-bold text-black"> Produits commande</TableHead>
+            <TableHead className="font-bold text-black"> Favoris</TableHead>
           </TableRow>
         </TableHeader>
 
@@ -26,102 +44,39 @@ const TableProduitComposant = () => {
             <TableCell>
               <Checkbox />
             </TableCell>
-            <TableCell className="font-medium">
-              <div className="w-10 h-10 rounded-full bg-gray-400 flex items-center justify-center text-black">
-                J
-              </div>
-            </TableCell>
-            <TableCell>
-              <span className="px-2 py-1 rounded-full text-xs font-medium bg-blue-100 text-blue-800">
-                Admin
-              </span>
-            </TableCell>
             <TableCell>Jean Dupont</TableCell>
-            <TableCell>jean@example.com</TableCell>
-            <TableCell>01/04/2024</TableCell>
+            <TableCell>
+             599 euros
+            </TableCell>
+            <TableCell><Badge className={prixPromotion ? "bg-green-500 text-white": "bg-red-500 text-white"}>{prixPromotion ? prixPromotion : "Pas en Promo"}</Badge></TableCell>
+            <TableCell><Badge className={GestionduStock()}> {QuantiteStock} </Badge></TableCell>
+            <TableCell><Badge className={promotion === true ? "bg-green-500 text-white": "bg-red-500 text-white"}>{promotion === true ? "Oui" : "Non"}</Badge></TableCell>
             <TableCell>
               <div className="flex items-center">
-                <span className="px-2 py-1 rounded-full text-xs font-medium bg-purple-100 text-purple-800">
-                  Premium
-                </span>
+               
                 <select className="ml-2 text-xs border rounded p-1">
-                  <option>Admin</option>
-                  <option>Utilisateur</option>
+                  <option>GAMING</option>
+                  <option>ELECTRONIQUE</option>
+                  <option>INFORMATIQUE</option>
+                  <option>MOBILIER</option>
                 </select>
               </div>
             </TableCell>
             <TableCell>
-              <span className="px-2 py-1 rounded-full text-xs font-medium bg-green-100 text-green-800">
-                année
-              </span>
+             3
+            </TableCell>
+            <TableCell>
+              <Badge className='bg-green-500'> Coucou</Badge>
+            </TableCell>
+            <TableCell>
+             5
+            </TableCell>
+            <TableCell>
+              6
             </TableCell>
           </TableRow>
 
-          <TableRow>
-            <TableCell>
-              <Checkbox />
-            </TableCell>
-            <TableCell className="font-medium">
-              <div className="w-10 h-10 rounded-full bg-gray-400 flex items-center justify-center text-black">
-                M
-              </div>
-            </TableCell>
-            <TableCell>
-              <span className="px-2 py-1 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-                Utilisateur
-              </span>
-            </TableCell>
-            <TableCell>Marie Martin</TableCell>
-            <TableCell>marie@example.com</TableCell>
-            <TableCell>15/03/2024</TableCell>
-            <TableCell>
-              <div className="flex items-center">
-                <span className="px-2 py-1 rounded-full text-xs font-medium bg-purple-100 text-purple-800">
-                  Premium
-                </span>
-                <select className="ml-2 text-xs border rounded p-1">
-                  <option>Admin</option>
-                  <option selected>Utilisateur</option>
-                </select>
-              </div>
-            </TableCell>
-            <TableCell>
-              <span className="px-2 py-1 rounded-full text-xs font-medium bg-yellow-100 text-yellow-800">
-                mois
-              </span>
-            </TableCell>
-          </TableRow>
-
-          <TableRow>
-            <TableCell>
-              <Checkbox />
-            </TableCell>
-            <TableCell className="font-medium">
-              <div className="w-10 h-10 rounded-full bg-gray-400 flex items-center justify-center text-black">
-                P
-              </div>
-            </TableCell>
-            <TableCell>
-              <span className="px-2 py-1 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-                Utilisateur
-              </span>
-            </TableCell>
-            <TableCell>Pierre Durand</TableCell>
-            <TableCell>pierre@example.com</TableCell>
-            <TableCell>20/02/2024</TableCell>
-            <TableCell>
-              <div className="flex items-center">
-                <span className="px-2 py-1 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-                  Free
-                </span>
-                <select className="ml-2 text-xs border rounded p-1">
-                  <option>Admin</option>
-                  <option selected>Utilisateur</option>
-                </select>
-              </div>
-            </TableCell>
-            <TableCell>Freemium</TableCell>
-          </TableRow>
+         
         </TableBody>
       </Table>
     </div>
