b213a73456a56a42287a0f529d5bf06017e7e061
YacineBoukliff <yacine69800@live.fr>
2025-04-21 13:51:26 +0200

    Ajout du hook pour ajouter des produits avec tanstack query plus qu'a implémenter ce hook avec react hook form et utiliser cloudinary pour l'upload d'image

--- E-commerce/app/dashboard/produits/(hooks)/UseProduits.ts
+++ E-commerce/app/dashboard/produits/(hooks)/UseProduits.ts
@@ -2,44 +2,77 @@ import { reponseApiProduit } from "@/app/api/admin/produits/(interface-types)/in
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import toast from "react-hot-toast";
-import { supprimerProduit } from "../(actions)/ActionProduit";
+import { ajouterProduit, supprimerProduit } from "../(actions)/ActionProduit";
 
 export function useProduits() {
-    return useQuery<reponseApiProduit[]>({  
-        queryKey: ['produits'],
-        queryFn: async () => {
-            const response = await axios.get<{ produits: reponseApiProduit[] }>("/api/admin/produits");
-            return response.data.produits; 
-        }
-    });
+  return useQuery<reponseApiProduit[]>({
+    queryKey: ["produits"],
+    queryFn: async () => {
+      const response = await axios.get<{ produits: reponseApiProduit[] }>(
+        "/api/admin/produits"
+      );
+      return response.data.produits;
+    },
+  });
 }
 
 export function useSupprimerProduits() {
-    const queryClient = useQueryClient();
-  
-    return useMutation({
-      mutationFn: async (produitId: string) => {
-        const result = await supprimerProduit(produitId);
-        return result;
-      },
-      onMutate: async (produitId) => {
-        await queryClient.cancelQueries({ queryKey: ['produits'] });
-        const anciensProduits = queryClient.getQueryData(['produits']);
+  const queryClient = useQueryClient();
 
-        queryClient.setQueryData(['produits'], (listeActuelle: reponseApiProduit[]) => 
-          listeActuelle.filter(produit => produit.id !== produitId)
-        );
-        
-        return { anciensProduits };
-      },
-      onError: (error: Error, ) => {
-        toast.error(error.message || "Erreur lors de la suppression du produit");
-      },
-      onSuccess: (data) => {
-        toast.success(data.message || "Produit supprimé avec succès");
-      },
-      onSettled: () => {
-        queryClient.invalidateQueries({ queryKey: ['produits'] });
+  return useMutation({
+    mutationFn: async (produitId: string) => {
+      const result = await supprimerProduit(produitId);
+      return result;
+    },
+    onMutate: async (produitId) => {
+      await queryClient.cancelQueries({ queryKey: ["produits"] });
+      const anciensProduits = queryClient.getQueryData(["produits"]);
+
+      queryClient.setQueryData(
+        ["produits"],
+        (listeActuelle: reponseApiProduit[]) =>
+          listeActuelle.filter((produit) => produit.id !== produitId)
+      );
+
+      return { anciensProduits };
+    },
+    onError: (error: Error) => {
+      toast.error(error.message || "Erreur lors de la suppression du produit");
+    },
+    onSuccess: (data) => {
+      toast.success(data.message || "Produit supprimé avec succès");
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["produits"] });
+    },
+  });
+}
+
+export function useAjouterProduit() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: reponseApiProduit) => {
+      const result = await ajouterProduit(data);
+      return result;
+    },
+
+    onMutate: () => {
+      toast.loading("Création du produit en cours ... ");
+    },
+
+    onSuccess: (data) => {
+      toast.dismiss();
+      if(data.success){
+        toast.success(data.message)
+        queryClient.invalidateQueries({queryKey: ["produits"]})
       }
-    });
+      else {
+        toast.error(data.message || "Echec de l'ajout du produit")
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["produits"] });
+    },
+  });
 }
