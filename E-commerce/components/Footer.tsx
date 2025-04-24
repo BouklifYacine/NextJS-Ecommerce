ec933340df117d52f67871c547e703486cec7912
YacineBoukliff <yacine69800@live.fr>
2025-04-24 13:55:55 +0200

    Début de la page d'accueil du e-commerce on va commencer par la layout avec la navbar et le footer

---
+++ E-commerce/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className='bg-gray-400 text-yellow-500'>
+        <div className='container mx-auto'>
+            <div className='flex justify-between'>
+<div className=' flex-col'>Yacine Salah </div>
+<div>Yacine Salah </div>
+<div>Yacine Salah </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Footer
