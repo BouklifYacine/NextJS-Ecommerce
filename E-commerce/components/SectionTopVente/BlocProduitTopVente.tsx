1a2a6b45f3e5d24bfeb58fc1727d866a95fa313c
YacineBoukliff <yacine69800@live.fr>
2025-04-25 11:53:56 +0200

    Ajout des ratings de merde avec Material UI et leur doc de CACA

--- E-commerce/components/SectionTopVente/BlocProduitTopVente.tsx
+++ E-commerce/components/SectionTopVente/BlocProduitTopVente.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
 
 const BlocProduitTopVente = () => {
   return (
@@ -7,13 +9,26 @@ const BlocProduitTopVente = () => {
         <div>
           <div className="bg-red-500 rounded-xl w-96 h-96">
             YACINE
-            <div className="flex justify-between px-4 ">
-              <p className="text-xl">Titre du produit </p>
-              <p className="text-xl">$89.99</p>
+            <div className="flex justify-between items-center px-4 ">
+              <p className="text-xl">Titre du produits </p>
+              <p className="text-xl">$89.990</p>
             </div>
-            <div className="flex justify-between px-4">
-              <p className="text-xl">Titre du produit </p>
-              <p className="text-xl">$89.99</p>
+            <div className="flex justify-between items-center px-4">
+              <p className="text-xl">Titre du produittt </p>
+              <div className="flex items-center gap-4">
+              <Rating
+  value={4}
+  precision={1}  // Active les demi-étoiles
+  max={5}
+  size="medium"
+  readOnly // Pas de hover effet sur les ratings 
+/>
+    
+  
+    <span>(2)</span>
+              </div>
+              
+          
             </div>
           </div>
         </div>
