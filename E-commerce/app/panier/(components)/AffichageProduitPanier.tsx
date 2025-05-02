dbf1ac9640d9aafb88eb6d914a4cf71e0e0852f1
YacineBoukliff <yacine69800@live.fr>
2025-05-02 11:04:53 +0200

    Affichage et calcul du prix total du panier de manière dynamiquement pour l'afficher sur le front je vais appliquer la tva maintenant

--- E-commerce/app/panier/(components)/AffichageProduitPanier.tsx
+++ E-commerce/app/panier/(components)/AffichageProduitPanier.tsx
@@ -12,12 +12,21 @@ const AffichageProduitPanier = () => {
 
   const produitPanier = data?.panier?.items;
 
+ const prixTotal = produitPanier?.reduce((acc, item) => {
+    const totalProduit = item.quantite * item.produit.prix
+    return acc + totalProduit
+ }, 0)
+
  
 
   return (
     <>
       <div className="container mx-auto">
         <h1>Mon Panier</h1>
+
+        <div>
+                <p>Prix total du panier : {prixTotal} euros</p>
+              </div>
        
         {produitPanier && produitPanier.length > 0 ? (
           produitPanier.map((produit) => (
@@ -27,9 +36,13 @@ const AffichageProduitPanier = () => {
               <p>Quantité: {produit.quantite}</p>
               <p>Prix unitaire: {produit.produit.prix} €</p>
               <p>Total pour cet article: {produit.quantite * produit.produit.prix} €</p>
+
+          
           
             </div>
           ))
+
+          
         ) : (
           <p>Votre panier est vide.</p>
         )}
